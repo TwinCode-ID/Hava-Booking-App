a3e50e97f363c685366435381e7615afe855afc0
Nadya2012 <nadya12.ang@gmail.com>
2025-12-13 16:34:29 +0700

    Home Screen

--- Frontend/hava-booking-app/src/App.jsx
+++ Frontend/hava-booking-app/src/App.jsx
@@ -8,16 +8,18 @@ import LandingPage from "./pages/LandingPage/LandingPage";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Auth/LogIn";
 import SignUp from "./pages/Auth/SignUp";
+import BookNow from "./pages/BookingPage/BookNow";
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path='*' element={<Navigate to='/' />} />
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/book-now" element={<BookNow />} />
         </Routes>
       </Router>
     </AuthProvider>
