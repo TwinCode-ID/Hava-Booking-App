34954ff554a0803a99318ebeae78b251d2037824
WILLIAM JONATHAN HANDOKO <108049958+WillieHandoko@users.noreply.github.com>
2025-12-18 21:28:12 +0700

    .

--- Frontend/hava-booking-app/src/components/LoadingSpinner.jsx
+++ Frontend/hava-booking-app/src/components/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import { Briefcase } from "lucide-react";
 
 const LoadingSpinner = () => {
   return (
-    <div className='min-h-screen bg-linear-to-br from-blue-50 via-white to-purple-50 flex items-center justify-center'>
+    <div className='container mx-auto bg-white flex items-center justify-center'>
       <div className='text-center'>
         <div className='relative'>
           <div className='animate-spin rounded-full h-16 w-16 border-4 border-emerald-200 border-t-emerald-600 mx-auto mb-4'></div>
