abfa1bcfb00b23b420a4314f35d7035bcc66f566
WILLIAM JONATHAN HANDOKO <108049958+WillieHandoko@users.noreply.github.com>
2025-12-19 21:54:27 +0700

    .

--- Backend/server.js
+++ Backend/server.js
@@ -30,5 +30,7 @@ app.use("/api/studio", studioRoutes);
 app.use("/api/package", packagesRoutes);
 app.use("/api/instructor", instructorsRoutes);
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
